8f1052339840a610226ce1168496b0a4894fdaf4
Paul Mason <latepaul@gmail.com>
2019-06-27 17:00:12 +0100

    Added lines to draw a simple form.

--- puttysessmgr.ps1
+++ puttysessmgr.ps1
@@ -6,3 +6,8 @@
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void] [System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form = New-Object system.Windows.Forms.Form
+$Form.Size = New-Object System.Drawing.Size(400,200)
+
+$Form.ShowDialog()
