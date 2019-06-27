ce8892821e5fdd57e396149fb6a44d68a83ab447
Paul Mason <latepaul@gmail.com>
2019-06-27 19:09:23 +0100

    Set up treeview with session names - respond to clicks with console message.
    TODO:
    
       - change click to drag n drop?
       - AfterSelected is preferred to NodeMouseClick according to random
       dude on the internet.
       - think about UI design

--- puttysessmgr.ps1
+++ puttysessmgr.ps1
@@ -8,6 +8,37 @@
 [void] [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Form = New-Object system.Windows.Forms.Form
-$Form.Size = New-Object System.Drawing.Size(400,200)
+$Form.Size = New-Object System.Drawing.Size(400,600)
+$tree = New-Object System.Windows.Forms.TreeView
 
+$tree.Size = '380,500'
+$tree.Location ='5,5'
+$tree.Name = "Putty Sessions"
+$sessions =  Get-ChildItem -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions  | % {split-path -leaf $_.Name}
+
+write-host "List of sessions:`n"
+foreach ($sess in $sessions) 
+{
+    $newnode=$tree.Nodes.Add("$sess","$sess") |Out-Null
+}
+
+function rightclick ($node) {
+$nodetext=$node.Text
+write-host "right-click on: $nodetext"
+}
+
+$tree.add_MouseDoubleClick({
+$nodetext=$this.SelectedNode.Text
+write-host "double-click on: $nodetext"
+})
+
+$tree.add_NodeMouseClick({
+$whichnode=$this.SelectedNode
+if ($_.Button -eq 'Right')
+{
+  rightclick($whichnode)
+}
+
+})
+$Form.controls.add($tree)
 $Form.ShowDialog()
