d7fa678a42bd044c15bcf557c33c9599e9e189f2
Paul Mason <latepaul@gmail.com>
2019-06-28 16:38:09 +0100

    A sort-of-working version in that it can launch putty. Can't actually group into categories yet though.
    Update README - add TODO

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# puttysessmgr
-Putty Session Manager
+# Putty Session Launcher
+
+## What it Does
+
+Putty Session Launcher will read in putty sessions from the registry and 
+create a diaglog box with a tree control. 
+
+The First 6 of these are added to categories (Red, Green, Blue). Think of these
+as folders. 
+
+If you double-click on a session name it will launch putty with that session.
+
+If you right-click it will open a dialog that will offer to change the 
+category of that item. Currently that doesn't actually do anything.
+
+## TODO
+
+In no particular order, as they say,
+
+* make the new categories active (i.e. it recreates the tree and re-displays
+* Add a drop-down list of current categories + "new category"
+* save/restore to a config file (registry?)
+* prompt if putty is not in expected path
+* make example category "Favourites" instead of Red, Green, Blue
+* UI polish
--- puttysessmgr.ps1
+++ puttysessmgr.ps1
@@ -5,31 +5,165 @@
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
 [void] [System.Windows.Forms.Application]::EnableVisualStyles()
+
+
+
+function prompt{
+    param([string] $title,
+          [string] $message,
+          [string] $default)
+    
+    $answer=$default
+    $prompt_label.Text = $message
+    $Form2.Text = $title
+    $txtbox.Text = $default 
+    $Form2.Location = $Form.Location
+    $Form2.ShowDialog() |Out-Null
+    return $txtbox.Text
+}
+function rightclick {
+param([System.Windows.Forms.TreeNode]$node)
+$nodetext=$node.Name
+$cat=$node_cats[$nodetext]
+if ($cat -eq "none")
+{
+   $cat = ""
+}
+
+write-host "right-click on: $nodetext (cat=$cat)"
+$prompt_msg = "Enter category for "+$nodetext+":"
+$text = prompt "Category" $prompt_msg $cat
+Write-Host "entered text = [$text]"
+
+}
+
+function launch {
+    param([System.Windows.Forms.TreeNode]$node)
+
+$tag = $node.Tag
+
+   if ($tag -eq "item")
+   {
+        $sess_name=$node.Text
+        $cmd="& "+ $putty_exe + " -load " + $sess_name
+        write-host "Launching: $cmd"
+        & $putty_exe -load $sess_name
+   }
+}
+
+function add_node {
+param([System.Windows.Forms.TreeNodeCollection]$rootnode,
+      [string] $category,
+      [string] $name)
+
+    $newnode = New-Object System.Windows.Forms.TreeNode
+    $newnode.Name = $name
+    $newnode.Text = $name
+    $newnode.Tag = "item"
+
+    if ($category -eq "none")
+    {
+        Write-Host "$name has no category, adding to root node"
+        $rootnode.Add($newnode) |out-null        
+    }
+    else
+    {
+        write-host "Searching for node called $category"
+        $rootnode | % {"...Name: {0} Text: {1}" -f $_.Name, $_.Text }
+        $addto_node = $rootnode | ? {$_.Name -eq $category}
+
+        if ($addto_node)
+        {
+            write-host "found node with $category - $addto_node.Name"
+            $addto_node.Nodes.Add($newnode) |Out-Null
+        }
+        else
+        {
+            Write-Host "adding new category node $category"
+            $newcatnode = New-Object System.Windows.Forms.TreeNode
+            $newcatnode.Text=$category
+            $newcatnode.Name=$category
+            $newcatnode.Tag = "category"
+            $rootnode.Add($newcatnode)|Out-Null
+            $addto_node = $newcatnode
+            $addto_node.Nodes.Add($newnode)|Out-Null
+       
+        }
+    }
+}
+
+$puttypath = "C:\Program Files\PuTTY"
+$putty_exe = $puttypath + "\putty.exe"
+$putty_exe 
 
 $Form = New-Object system.Windows.Forms.Form
 $Form.Size = New-Object System.Drawing.Size(400,600)
-$tree = New-Object System.Windows.Forms.TreeView
-
-$tree.Size = '380,500'
-$tree.Location ='5,5'
-$tree.Name = "Putty Sessions"
-$sessions =  Get-ChildItem -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions  | % {split-path -leaf $_.Name}
-
-write-host "List of sessions:`n"
-foreach ($sess in $sessions) 
+$Form.Text = "Putty Session Launcher"
+$putty_icon = [System.Drawing.Icon]::ExtractAssociatedIcon($putty_exe)
+if ($putty_icon)
 {
-    $newnode=$tree.Nodes.Add("$sess","$sess") |Out-Null
+$Form.Icon = $putty_icon
+}
+else
+{
+Write-Host "No icon"
 }
 
-function rightclick ($node) {
-$nodetext=$node.Text
-write-host "right-click on: $nodetext"
+$tree = New-Object System.Windows.Forms.TreeView
+
+$tree.Size = '370,500'
+$tree.Location ='5,5'
+$tree.Text = "Putty Sessions"
+$tree.Font = '"Consolas",10'
+
+$categories=@()
+$node_cats=@{}
+
+$sessions =  Get-ChildItem -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions  | % {split-path -leaf $_.Name}
+
+$i=0 
+foreach ($sess in $sessions) 
+{
+    if ($i -lt 2)
+    {
+        $cat = "Red"
+    } 
+    elseif ($i -lt 4)
+    {
+        $cat = "Blue"
+    }
+    elseif ($i -lt 7)
+    {
+        $cat = "Green"
+    }
+    else
+    {
+        $cat = "none"
+    }
+
+    $i +=1
+
+    $new_cat = $categories | ? {$_ -eq $cat }
+    if (-not $new_cat)
+    {
+    $categories += $cat
+    }
+    $node_cats.add($sess,$cat) 
+}
+
+$i=0 
+foreach ($sess in $sessions) 
+{
+    write-host "`nAdding node - Session=[$sess] cat=[$($node_cats[$sess])]"
+    
+    add_node $tree.Nodes  $($node_cats[$sess]) $sess
+    $i += 1
 }
 
 $tree.add_MouseDoubleClick({
-$nodetext=$this.SelectedNode.Text
-write-host "double-click on: $nodetext"
+launch($this.SelectedNode)
 })
 
 $tree.add_NodeMouseClick({
@@ -41,4 +175,39 @@ if ($_.Button -eq 'Right')
 
 })
 $Form.controls.add($tree)
+
+$close_btn = New-Object System.Windows.Forms.Button
+$close_btn.location = '160,520'
+$close_btn.size = '80,25'
+$close_btn.Text = 'Close'
+$close_btn.Font = '"Arial",10'
+$Form.Controls.Add($close_btn)
+
+$Form2 = New-Object system.Windows.Forms.Form
+$Form2.Size = New-Object System.Drawing.Size(300,120)
+$Form2.Text = "question"
+$Form2.FormBorderStyle='FixedDialog'
+
+$txtbox = New-Object System.Windows.Forms.TextBox
+$txtbox.size = '270,100'
+$txtbox.Location = '15,30'
+$prompt_label = New-Object System.Windows.Forms.Label
+$prompt_label.Text = "Enter"
+$prompt_label.location = '5,5'
+$prompt_label.Size = '280,20'
+$Form2.Controls.Add($prompt_label) 
+$Form2.Controls.Add($txtbox)
+
+$close_btn2 = New-Object System.Windows.Forms.Button
+$close_btn2.location = '150,60'
+$close_btn2.size = '40,25'
+$close_btn2.Text = 'OK'
+$close_btn2.Font = '"Arial",8'
+$Form2.Controls.Add($close_btn2)
+$close_btn2.Add_Click({$Form2.Close()})
+
+$close_btn.Add_Click({$Form.Close()
+$form2.Close()})
+
 $Form.ShowDialog()
+$form2.close()
